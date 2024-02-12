196d9f7e20f02150df3adbba2dd5925a796aa058
Zane Gray <zgraywilbur@gmail.com>
2024-02-12 10:34:29 -0800

    Load button visibility
    Added a check for save_file to load button and main menu.

--- src/start_menu.gd
+++ src/start_menu.gd
@@ -2,14 +2,27 @@ extends Control
 
 @onready var battle_scene = preload("res://MainScenes/battle.tscn") as PackedScene
 @onready var confirmation_dialog = $ConfirmationDialog
+@onready var load_game = $MarginContainer/HBoxContainer/VBoxContainer/load_game
+
+func _ready():
+	var save_file = FileAccess.open(State.FILE_PATH, FileAccess.READ)
+	if save_file:
+		load_game.visible = true
+		save_file.close
+	else:
+		load_game.visible = false
 
 func _on_start_new_game_pressed():
 	State.initialize_player_data()
 	get_tree().change_scene_to_packed(battle_scene)
 
 func _on_load_game_pressed():
-	State.load_player_data()
-	get_tree().change_scene_to_file("res://MainScenes/campfire.tscn")
+	var save_file = FileAccess.open(State.FILE_PATH, FileAccess.READ)
+	if save_file:
+		State.load_player_data()
+		get_tree().change_scene_to_file("res://MainScenes/campfire.tscn")
+	else:
+		print("Player data not found.")
 
 func _on_quit_game_pressed():
 	confirmation_dialog.popup()  
