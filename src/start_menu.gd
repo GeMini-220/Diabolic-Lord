8d288e25ab3425fcc082377bdc5ca9a82bdc7815
ShadowGenius <86333979+ShadowGenius@users.noreply.github.com>
2024-02-11 22:03:54 -0800

    Revert "Changer_Scence"

--- src/start_menu.gd
+++ src/start_menu.gd
@@ -1,10 +1,11 @@
 extends Control
 
 @onready var battle_scene = preload("res://MainScenes/battle.tscn") as PackedScene
- @onready var confirmation_dialog = $ConfirmationDialog
+@onready var confirmation_dialog = $ConfirmationDialog
 
 func _on_start_new_game_pressed():
-	get_tree().change_scene_to_packed(battle_scene)	
+	get_tree().change_scene_to_packed(battle_scene)
+
 
 func _on_quit_game_pressed():
 	confirmation_dialog.popup()  
